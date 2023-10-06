5ae9c12bf1ee8a0901661714e31db0c0ec82d303
ArghyaLalMitra <145666315+ArghyaLalMitra@users.noreply.github.com>
2023-10-06 12:46:41 +0530

    Add files via upload

---
+++ 002. Add Two Numbers.py
@@ -0,0 +1,24 @@
+#CODE 1
+# This program adds two numbers
+x = 1.5
+y = 6.3
+
+# Add two numbers
+sum = x + y
+
+# Display the sum
+print('The sum of {0} and {1} is {2}'.format(x, y, sum))
+
+#CODE 2
+# Store input numbers
+a = input('Enter first number: ')
+b = input('Enter second number: ')
+
+# Add two numbers
+sum = float(a) + float(b)
+
+# Display the sum
+print('The sum of {0} and {1} is {2}'.format(a, b, sum))
+
+#CODE 3
+print('The sum is %.1f' %(float(input('Enter first number: ')) + float(input('Enter second number: '))))
